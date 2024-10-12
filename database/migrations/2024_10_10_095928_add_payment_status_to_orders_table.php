1d1397ac881eb87069dec2b2832acabcbcbc707f
Shivika-Rawat <rawatshivika18@gmail.com>
2024-10-12 11:20:07 +0530

    Add files via upload

---
+++ database/migrations/2024_10_10_095928_add_payment_status_to_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddPaymentStatusToOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->string('payment_status')->after('product_id')->default('cash on delivery');
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->drop('payment_status');
+            //
+        });
+    }
+}
